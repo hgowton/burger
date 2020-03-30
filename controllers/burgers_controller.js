049d91fa74d7f41233c8e1a944dc235ed0a5e809
hgowton <hgowton@gmail.com>
2020-03-30 14:28:11 -0400

    added handlebars and fixed files in controllers, models, and config directories

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 //Import the burger.js to use its database functions
-var burger = require("..models/burger.js");
+var burger = require("../models/burger.js");
 
 //Create all our routes and set up logic within those routes where required
 router.get("/", function (req, res) {
@@ -19,21 +19,43 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
     burger.insertOne([
-
-    ])
-})
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        //Send back the ID of the new burger
+        res.json({id: result.insertId});
+    });
+});
 
 router.put("/api/burgers/:id", function(req, res){
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
 
     burger.updateOne({
-        devour: req.body.devour
+        devoured: req.body.devoured
     }, condition, function(result) {
         if (result.changedRows == 0){
+            //if no rows are changed, then the ID does not exist
             return res.status(404).end();
         } else {
             res.status(200).end();
         }
     });
 });
+
+router.cancelOne("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.cancelOne(condition, function(result){
+        if (result.affectedRows == 0) {
+            //if no rows are deleted, then the ID does not exits
+            return res.status(404).end()
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//Export routes for server.js to use
+module.exports = router;
