fc8ff424a7548ffc741a3bec636865e42f1d8d36
hgowton <hgowton@gmail.com>
2020-03-31 18:42:10 -0400

    working on fixing devour and reorder

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
         var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
+        // console.log(hbsObject);
         res.render("index", hbsObject);
     })
 
@@ -30,10 +30,10 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function(req, res){
     var condition = "id = " + req.params.id;
-    console.log("condition", condition);
+    console.log("condition " + condition)
 
     burger.updateOne({
-        devoured: req.body.devoured
+        devoured: parseInt(req.body.devoured)
     }, condition, function(result) {
         if (result.changedRows == 0){
             //if no rows are changed, then the ID does not exist
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,19 +2,19 @@
 $(function() {
     $(".change-devoured").on("click", function(event) {
         var id = $(this).data("id");
-        var newDevoured = $(this).data("newdevoured");
-
+        var updateDevour = !$(this).data("newdevour");
+ 
         var newDevouredState = {
-            devoured : newDevoured
+            devoured : updateDevour
         };
 
         //Send the PUT request
-        $.ajax("/api/burgers" + id, {
+        $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevouredState
         }).then(
             function() {
-                console.log("changed devoured to" , newDevoured);
+                console.log("changed devoured to" , updateDevour);
                 //Reload the page to get the updated list
                 location.reload();
             }
