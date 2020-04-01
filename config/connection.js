b5a128738dd06004b42b6b7b666c191a81d1b74e
hgowton <hgowton@gmail.com>
2020-03-31 22:07:39 -0400

    updated connection page with to connect to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //Set up connection to mysql
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "test",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnectioncreateConnection({
+        host: "localhost",
+        port: 3306,
+        user: "test",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
 //Create the connection to the burgers_db in mysql
 connection.connect(function(err) {
