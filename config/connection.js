ac828d23e5c11f039177ce3781313044720798db
hgowton <hgowton@gmail.com>
2020-04-01 16:56:49 -0400

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnectioncreateConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "test",
